333ae20fd3bf90e1ce5c9ff07d7aea22bd044127
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-23 14:15:45 -0300

    .

--- rohp.py
+++ rohp.py
@@ -5,10 +5,10 @@ def principal():
     st.write('''A ferramenta ROHP é uma ferramenta que realizará o diagnóstico de sua organização/setor a fim de conhecer sua situação atual, isto é, qual é o seu grau de maturidade em Gestão de Conhecimento (GC) e, assim, possa definir seu planejamento para implementar ou aperfeiçoar a GC.''')
     st.subheader('Instruções para preenchimento')
     '''Cada uma das 8 características presentes nas 4 dimensões deve ser minuciosamente analisada e pontuada com base em evidências, utilizando a seguinte escala:
-    1= Discordo Totalmente
-    2= Discordo
-    3= Discordo Parcialmente
-    4= Concordo
-    5= Concordo Totalmente 
+    \n\n1= Discordo Totalmente
+    \n2= Discordo
+    \n3= Discordo Parcialmente
+    \n4= Concordo
+    \n5= Concordo Totalmente 
     '''
 principal()
