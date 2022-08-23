3f743df46e537727c1885e82a4336a05ffc25c9e
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-23 18:59:58 -0300

    .

--- rohp.py
+++ rohp.py
@@ -14,9 +14,9 @@ def principal():
     st.write('##')
 
     if st.button('Iniciar diagnóstico'):
-        relacional()
+        diagnostico()
 
-def relacional():
+def diagnostico():
     st.title('Diagnóstico')
     st.write('')
 
@@ -85,8 +85,7 @@ def relacional():
         with c2:
             st.write('')
         with c3:
-            avancar = st.form_submit_button('Avançar'):
-            organizacional()
+            avancar = st.form_submit_button('Avançar')
         
 def organizacional():
     st.title('Diagnóstico')
