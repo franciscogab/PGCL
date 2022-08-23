a1e75b1bf2df8bc3611226eadff5b8a0989e6774
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-23 14:12:19 -0300

    .

--- rohp.py
+++ rohp.py
@@ -3,5 +3,5 @@ import streamlit as st
 def principal():
     st.title('ROHP')
     st.write('''A ferramenta ROHP é uma ferramenta que realizará o diagnóstico de sua organização/setor a fim de conhecer sua situação atual, isto é, qual é o seu grau de maturidade em Gestão de Conhecimento (GC) e, assim, possa definir seu planejamento para implementar ou aperfeiçoar a GC.''')
-
+    st.subheader('Instruções para preenchimento')
 principal()
