47d519c4990f0264ae5358f0dd74caf187820fb3
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-23 23:02:02 -0300

    .

--- rohp.py
+++ rohp.py
@@ -336,6 +336,7 @@ def processual():
 
 def formulario():
     st.title('Formulário')
+    st.set_page_config(layout='centered')
     with st.form('form'):
         om = st.text_input('OM')
         nome = st.text_input('Nome Completo')
