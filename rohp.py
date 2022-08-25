98232f6dcbbbb061f88afae14ede8f7464056d71
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-25 18:54:57 -0300

    .

--- rohp.py
+++ rohp.py
@@ -509,7 +509,7 @@ def relatorio():
     col1, col2, col3 = st.columns(3)
     with col3:
         st.markdown('''<p style='text-align:right'>Emissão: {}</p><br>
-                    <hr>
+                    <hr style='border:5px'>
                     <p style='text-align:center; font-size:25px'>{}<br>
                     {}<br>
                     {}</p>'''.format(datetime.now().strftime('%d/%m/%Y'), st.session_state['nome'], st.session_state['posto'], st.session_state['funcao']), unsafe_allow_html=True)
