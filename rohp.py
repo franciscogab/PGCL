eb2905864c93fd5329075714f1e688a1f9829ebf
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-25 17:16:24 -0300

    .

--- rohp.py
+++ rohp.py
@@ -16,8 +16,7 @@ def principal():
     st.write('''A Matriz ROHP é uma ferramenta que realizará o diagnóstico de sua organização/setor, a fim de identificar qual é o seu grau de maturidade em Gestão de Conhecimento (GC) e, assim, definir seu planejamento para implementar ou aperfeiçoar a GC.''')
     st.write('##')
 
-    st.markdown("<a href='https://www.google.com'><img src='https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Imagens%2Fpgclnovo.png?alt=media&token=f1a8b5d3-6f22-4ac6-a758-98ee17f4125d' alt='PGCL' style='width:10%'></a>", unsafe_allow_html=True)
-    st.write('##')
+    st.markdown("<a href='https://www.google.com'><img src='https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Imagens%2Fpgclnovo.png?alt=media&token=f1a8b5d3-6f22-4ac6-a758-98ee17f4125d' alt='PGCL' style='width:25%'></a>", unsafe_allow_html=True)
     st.write('##')
 
     st.subheader('Instruções para preenchimento')
