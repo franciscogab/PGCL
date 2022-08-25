31922ed9ac0e7dd59ec60149a4cb271cfffc0380
franciscogab <111901234+franciscogab@users.noreply.github.com>
2022-08-25 18:31:48 -0300

    .

--- rohp.py
+++ rohp.py
@@ -508,7 +508,7 @@ def relatorio():
     st.write('##')
 
     
-        st.markdown("<a href='https://franciscogab-pgcl-rohp-n7szap.streamlitapp.com/'><button style='display:block; margin-left:auto; margin-right:auto'>Novo Relatório</button></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://franciscogab-pgcl-rohp-n7szap.streamlitapp.com/'><button style='display:block; margin-left:auto; margin-right:auto'>Novo Relatório</button></a>", unsafe_allow_html=True)
         
 
         
